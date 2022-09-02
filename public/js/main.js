7a62b33fb0b0e6692c2f54af8e7abfe9576e9744
Emily Reed <emilyreed89@gmail.com>
2022-09-01 21:29:24 -0400

    adding saved

--- public/js/main.js
+++ public/js/main.js
@@ -14,6 +14,55 @@ Array.from(todoComplete).forEach((el)=>{
     el.addEventListener('click', markIncomplete)
 })
 
+function edit(id, event) {
+    let parentElm = event.target.closest("li")
+    let contentElm = parentElm.querySelector(".content")
+    //this is the magic that allows the super clean edit on page
+    contentElm.setAttribute("contenteditable", true)
+    parentElm.classList.add("editing")
+  }
+  
+
+async function save(id, event) {
+  //so leon uses below to grab id since its directly near no containers or elements between
+  
+  //need to grab the value out of .content
+  const todoId = this.parentNode.dataset.id
+  
+    // let parentElm = event.target.closest("li")
+    console.log(parentElm)
+    //the element within that parentElm's
+    let contentElm = parentElm.querySelector(".content")
+    console.log(contentElm)
+
+    let content = contentElm.innerText
+  
+ 
+    let rawResponse = await fetch("/save", {
+  
+  
+      method: "POST",
+      headers: {
+        Accept: "application/json",
+        "Content-Type": "application/json"
+      },
+
+      body: JSON.stringify({ id: id, content: content })
+    })
+  
+    if (rawResponse.status == 200) {
+      parentElm.classList.remove('editing')
+
+    
+    } else { // everything is not good
+      console.log(rawResponse)
+
+      alert("something went wrong in the server")
+
+    }
+
+}
+
 async function deleteTodo(){
     const todoId = this.parentNode.dataset.id
     try{
