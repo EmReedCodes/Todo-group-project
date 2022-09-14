6002a2d55e8945241cde504c4d66e65c895985c5
BryceArchibald <brycekarchibald@gmail.com>
2022-09-14 13:46:38 -0700

    Added default priority of normal to createTodo function

--- controllers/todos.js
+++ controllers/todos.js
@@ -33,7 +33,7 @@ module.exports = {
   },
   createTodo: async (req, res) => {
     try {
-      await Todo.create({ todo: req.body.todoItem, completed: false, userId: req.user.id })
+      await Todo.create({ todo: req.body.todoItem, completed: false, userId: req.user.id, priority: "Normal" })
       console.log('Todo has been added!')
       res.redirect('/todos')
     } catch (err) {
